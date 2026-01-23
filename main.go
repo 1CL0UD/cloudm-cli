4d38898df9f41ec8e2dcaf23e5af6f55dd11fd9f
Shalahuddin <shalahuddin.tech@gmail.com>
2026-01-24 00:19:29 +0700

    feat: revamp

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"os"
-
-	"github.com/1CL0UD/cloudm-cli/cmd"
-)
-
-// Version info - set via ldflags during build
-var (
-	Version   = "dev"
-	BuildDate = "unknown"
-	GitCommit = "unknown"
-)
-
-func init() {
-	// Set version info in cmd package
-	cmd.Version = Version
-	cmd.BuildDate = BuildDate
-	cmd.GitCommit = GitCommit
-}
+import "fmt"
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
-	}
+	fmt.Println("cloudm-cli: A barebones CLI tool")
 }
